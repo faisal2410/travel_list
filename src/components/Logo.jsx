24b313fb2ef339cde356eb142a0ced702d40cc9f
Faisal ahmed <faisal2410@yahoo.com>
2023-07-13 14:47:12 +0600

    initial commit

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,3 +1,5 @@
+
+
 export default function Logo() {
     return <h1>🏝️ Far Away 🧳</h1>;
 }
--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 export default function Stats({ items }) {
     if (!items.length)
         return (
